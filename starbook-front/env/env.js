8ceff37a1a994fd9f2b59f4d68840f616dc85610
ohadsc <ohad.schachtel@gmail.com>
2016-08-26 17:55:10 +0300

    feat(update skills): enable update of skills and hobbies

--- starbook-front/env/env.js
+++ starbook-front/env/env.js
@@ -1,6 +1,9 @@
+STAR_BOOK_API = "http://192.168.99.100:5000/api";
+SEND_COOKIES = false;
+
 angular.module('myApp')
   .constant("ENV", {
     "GOOGLE_SIGN_IN_CLIENT_ID": "some-client-id.apps.googleusercontent.com",
-    "STAR_BOOK_API": "http://localhost:5000/api",
-    "SEND_COOKIES": false
+    "STAR_BOOK_API": STAR_BOOK_API,
+    "SEND_COOKIES": SEND_COOKIES
   });
