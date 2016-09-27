299a4c04c51121b12aa674d03a06a845dfe85d77
Ariel Cabib <ariel.cabib@gmail.com>
2016-09-27 12:54:15 +0300

    not tagging git

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -9,10 +9,6 @@ sudo docker push 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:${
 
 if [[ $TRAVIS_BRANCH == "master" ]]; then
   echo "Yay, it's release branch, let's tag it and bump docker images!"
-  git config user.email "travis-ci@yotpo.com"
-  git config user.name "Travis CI"
-  git tag -a "v-${BUILD_DATE}" -m "Committed by Travis-CI ${TRAVIS_BUILD_NUMBER} [ci skip]"
-  git push --tags
 
   sudo docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:v-${BUILD_DATE}
   sudo docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:latest
