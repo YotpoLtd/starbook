f405c0df928ecc72bc6125f693be9ae2c158ddf3
Andrei Burd <bandrei@yotpo.com>
2017-06-11 21:17:04 +0300

    moving to nomad

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -2,21 +2,26 @@
 #
 # Tag and push git and Docker
 
-sudo docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:${TRAVIS_BRANCH}
-sudo docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:${TRAVIS_BRANCH}
-sudo docker push 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:${TRAVIS_BRANCH}
-sudo docker push 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:${TRAVIS_BRANCH}
+set -x
+set -i
+
+docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:${TRAVIS_BRANCH}
+docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:${TRAVIS_BRANCH}
+docker push 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:${TRAVIS_BRANCH}
+docker push 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:${TRAVIS_BRANCH}
 
 if [[ $TRAVIS_BRANCH == "master" ]]; then
   echo "Yay, it's release branch, let's tag it and bump docker images!"
 
-  sudo docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:v-${BUILD_DATE}
-  sudo docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:latest
-  sudo docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:v-${BUILD_DATE}
-  sudo docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:latest
-  sudo docker push 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:v-${BUILD_DATE}
-  sudo docker push 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:latest
-  sudo docker push 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:v-${BUILD_DATE}
-  sudo docker push 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:latest
+  echo "Pushing images latest and v-${BUILD_DATE} images"
+
+  docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:v-${BUILD_DATE}
+  docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:latest
+  docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:v-${BUILD_DATE}
+  docker tag 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:${TRAVIS_COMMIT} 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:latest
+  docker push 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:v-${BUILD_DATE}
+  docker push 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/front:latest
+  docker push 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:v-${BUILD_DATE}
+  docker push 402837048690.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/back:latest
 
 fi
